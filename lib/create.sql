beaf0ae77fa79773a68357f47ee7cbe96ebe4926
nyinyilayhtet <nnlayhtet@gmail.com>
2022-06-10 13:15:35 -0400

    commit tree e989c1eb6b2f9647d88302383204447462838338

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+--   sex CHAR(1),
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "very calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "very intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "very Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "very Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "very goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "very Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "very naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "very aggressive", 0);
